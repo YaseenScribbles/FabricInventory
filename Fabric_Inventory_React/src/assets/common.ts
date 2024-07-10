d51a2da73ae36daea488216478c0465f7edee826
YaseenScribbles <yaseen0696@gmail.com>
2024-07-10 18:46:33 +0530

    URL Update

--- Fabric_Inventory_React/src/assets/common.ts
+++ Fabric_Inventory_React/src/assets/common.ts
@@ -1,1 +1,3 @@
-export const LOCAL_URL = 'http://192.168.10.65:8000/api';
+// export const LOCAL_URL = 'http://192.168.10.65:8000/api';
+export const LOCAL_URL = 'http://liveserver/fabric_inventory/api';
+
